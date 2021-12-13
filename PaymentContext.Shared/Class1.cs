fbf775188e8eb6ba0176389048cdc759b1541a73
Juliano Dibai <81191797+julianodibai@users.noreply.github.com>
2021-12-13 13:34:01 -0300

    Organizando as solucões

---
+++ PaymentContext.Shared/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+//O Shared são itens para compartilhar entre domains(dominios) obs: usado quando se tem multiplos dominios
+namespace PaymentContext.Shared
+{
+    public class Class1
+    {
+    }
+}
